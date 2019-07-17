8171e6e9b6adc2f8b4db27a5f6c84e1acb9e4e58
Aaron Gilman <gilmanaa2@gmail.com>
2019-07-17 15:10:36 +0300

    added server file

---
+++ main.py
@@ -0,0 +1,32 @@
+from bottle import route, run, template, static_file, request, response, redirect, TEMPLATE_PATH
+import os
+import pymysql
+import json
+
+TEMPLATE_PATH.insert(0, os.path.dirname(__file__))
+
+@route('/map')
+def map():
+    return template('./map.html')
+
+@route('/')
+def index():
+    return template('./index.html')
+
+@route('/<filename:re:.*\.css>')
+def styles(filename):
+    return static_file(filename, root="")
+
+@route('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root="")
+
+@route('/<filename:re:.*\.(jpg|jpeg|png|gif|ico)>')
+def images(filename):
+    return static_file(filename, root="")
+
+def main():
+    run(host='localhost', port=7000)
+
+if __name__ == '__main__':
+    main()
