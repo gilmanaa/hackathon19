50b6ef1c79998f445d2c758ca7ebc973cc3c72b7
Aaron Gilman <gilmanaa2@gmail.com>
2019-07-17 16:55:33 +0300

    added code for getting google routes lat and lngs also got polyline code

--- js/index.js
+++ js/index.js
@@ -11,24 +11,53 @@ function initMap() {
     directionsDisplay.setMap(map);
     var origin = 'oak park, il';
     var destination = 'chicago, il';
+    var routeCoordinates = []
     for (let i = 0; i < travelModes.length; i++) {
         directionsService.route({
             origin: origin,
             destination: destination,
             travelMode: travelModes[i]
         }, function (response, status) {
-            if (status === 'OK'&& i === 0) {
-                directionsDisplay.setDirections(response);
-                console.log(response);
-                console.log(response.routes[0].legs[0].steps[0].lat_lngs[0].lat());
-                console.log(response.routes[0].legs[0].steps[0].lat_lngs[0].lng());
-            } else if (status === 'OK') {
-                console.log(response.routes[0].legs[0].steps);
+            if (status === 'OK') {
+                var lat_lngs = []
+                for (let j = 0; j < response.routes[0].overview_path.length; j++) {
+                    var lat = response.routes[0].overview_path[j].lat()
+                    var lng = response.routes[0].overview_path[j].lng()
+                    var newCoord = {'lat':lat,'lng':lng}
+                    lat_lngs.push(newCoord);
+                }
+                var type = travelModes[i]
+                var travel = {}
+                travel[type] = lat_lngs
+                routeCoordinates.push(travel)
+                //directionsDisplay.setDirections(response);
             } else {
                 window.alert('Directions request failed due to ' + status);
             }
         });
     }
+    console.log(routeCoordinates);
+}
+
+function newRoute() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 32.109, lng: 34.855 },
+        zoom: 8
+    });
+    var routeCoordinates = [
+        {lat: 37.772, lng: -122.214},
+        {lat: 21.291, lng: -157.821},
+        {lat: -18.142, lng: 178.431},
+        {lat: -27.467, lng: 153.027}
+      ];
+      var newPath = new google.maps.Polyline({
+        path: routeCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      });
+      newPath.setMap(map);
 }
 
 function openNav() {
