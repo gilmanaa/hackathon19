0e795111cfc4bdced0e6d8465a175c2031d40e25
Aaron Gilman <gilmanaa2@gmail.com>
2019-07-18 10:21:53 +0300

    added server side sqlite db for route data

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bottle import route, run, template, static_file, request, response, redirec
 import os
 import pymysql
 import json
+import sqlite3
 
 TEMPLATE_PATH.insert(0, os.path.dirname(__file__))
 
@@ -9,10 +10,38 @@ google_route_data = {}
 origin = {}
 destination = {}
 
+
 @route('/gatherRouteData',method="POST")
 def gatherRouteData():
+    conn = sqlite3.connect('routeDB.sqlite')
+    cur = conn.cursor()
     data = request.POST.get('route')
     google_route_data['data'] = json.loads(data)
+    cur.executescript('''
+    Drop Table If Exists WALKING;
+    Drop Table If Exists BICYCLING;
+    Drop Table If Exists DRIVING;
+    Drop Table If Exists TRANSIT;
+    Drop Table If Exists TWO_WHEELER;
+    Create Table WALKING (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, lat DECIMAL(4,3), lng DECIMAL(4,3));
+    Create Table BICYCLING (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, lat DECIMAL(4,3), lng DECIMAL(4,3));
+    Create Table DRIVING (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, lat DECIMAL(4,3), lng DECIMAL(4,3));
+    Create Table TRANSIT (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, lat DECIMAL(4,3), lng DECIMAL(4,3));
+    Create Table TWO_WHEELER (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, lat DECIMAL(4,3), lng DECIMAL(4,3));
+    ''')
+    for data in google_route_data['data']:
+        for loc in data:
+            for lo in data[loc]:
+                lat = lo['lat']
+                lng = lo['lng']
+                cur.execute('Insert Into {0} Values (null,{1},{2})'.format(loc,lat,lng))
+    conn.commit()
+    cur.close()
 
 @route('/map')
 def map():
